16f6329afe777c979d389a846a7499ce34131ea1
devcloudengineering <devcloudengineering@gmail.com>
2024-06-21 21:39:07 -0400

    Se actualiza diseños sobre tarjetas publicitarias

--- src/components/Boton/Boton.jsx
+++ src/components/Boton/Boton.jsx
@@ -1,7 +1,3 @@
 export function Boton({ text }) {
-  return (
-    <button className="bg-lime-400 text-black font-bold font-serif p-2 rounded-md cursor-pointer hover:bg-lime-600 w-fit border">
-      {text}
-    </button>
-  );
+  return <button className="button-green">{text}</button>;
 }
--- src/components/CardPublicaciones/CardPublicaciones.jsx
+++ src/components/CardPublicaciones/CardPublicaciones.jsx
@@ -2,7 +2,7 @@ import { Boton } from "../Boton/Boton";
 
 const CardPublicaciones = ({ titulo, fecha, body, src, descripcion }) => {
   return (
-    <div className="flex flex-col w-[28%] p-4 items-center justify-center shadow border cursor-pointer">
+    <div className="flex flex-col w-[28%] p-4 items-center justify-center shadow border cursor-pointer transition-all scale-100 hover:scale-105 bg-lime-50">
       <img
         src={src}
         className="object-cover h-[150px] w-full rounded mb-2"
