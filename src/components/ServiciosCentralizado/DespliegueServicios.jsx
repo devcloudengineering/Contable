e88943da8ac36409092179f1573b57cb57c3e678
devcloudengineering <devcloudengineering@gmail.com>
2024-06-27 20:26:49 -0400

    Se promueve cursor pointer al div central

--- src/components/ServiciosCentralizado/DespliegueServicios.jsx
+++ src/components/ServiciosCentralizado/DespliegueServicios.jsx
@@ -2,12 +2,12 @@ import { Boton } from "../Boton/Boton.jsx";
 
 const DespliegueServicios = (props) => {
   return (
-    <div className="container mx-auto p-2 col-[2/5]">
+    <div className="container mx-auto p-2 col-[2/5] ">
       <div className="grid gap-6 grid-cols-2">
         {props.valorData.map((elemento) => (
           <div
             key={elemento.id}
-            className="grid grid-cols-4 gap-2 mb-4 bg-lime-200 p-2 rounded-lg w-[25rem] hover:bg-lime-300 hover:transition-all hover:scale-105"
+            className="grid grid-cols-4 gap-2 mb-4 bg-lime-200 p-2 rounded-lg w-[25rem] hover:bg-lime-300 hover:transition-all hover:scale-105 cursor-pointer"
           >
             <div className="flex justify-center items-center col-span-1">
               <img className="h-14" src={elemento.src} alt={elemento.titulo} />
@@ -20,7 +20,7 @@ const DespliegueServicios = (props) => {
               <h2 className="text-lg font-semibold">{elemento.precio}</h2>
               <Boton
                 text="Ver más"
-                classOptional="bg-lime-400 text-black text-xs font-bold font-serif px-2 py-2 rounded-md cursor-pointer hover:bg-lime-600 w-fit border tracking-wider transition-all"
+                classOptional="bg-lime-400 text-black text-xs font-bold font-serif px-2 py-2 rounded-md  hover:bg-lime-600 w-fit border tracking-wider transition-all"
               />
             </div>
           </div>
